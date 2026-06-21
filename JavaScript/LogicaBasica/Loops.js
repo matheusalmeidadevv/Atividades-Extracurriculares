e645c0e9d368470822875ad7666b1c237d77a603
Matheus DEV <matheusalmeida.devv@gmail.com>
2026-06-21 18:58:24 -0300

    Aprendendo JavaScript - Loops

---
+++ JavaScript/LogicaBasica/Loops.js
@@ -0,0 +1,16 @@
+//Não tem muitos comentarios porque a sintaxe é muito parecida com a do Java
+
+/*
+        DESAFIO
+    Criar um programa que imprima a tabuada de 1 até 10
+*/
+
+for(let i = 1; i <= 10; i++){
+    for(let o = 1; o <= 10; o++){
+
+        let num  = i * o;
+        console.log(`${i} X ${o} = ${num}`);
+
+    }
+
+}
